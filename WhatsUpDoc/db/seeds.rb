ad0e45278db6e73e770b742fec3c538421744e4e
TheWalkingNev <alexandre.gasior@gmail.com>
2019-02-01 00:57:47 +0100

    J'ai toujours très faim T_T

--- WhatsUpDoc/db/seeds.rb
+++ WhatsUpDoc/db/seeds.rb
@@ -1,7 +1,96 @@
 require 'faker'
 
+
 puts `clear`
-puts "Suppression de la base de données en cours..."
+puts ""
+puts "DATABASE DELETION. PLEASE BE PATIENT."
+puts ""
+
+puts "NNNNNNNNMMNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNd::::::::::oh/`./sssssymh+.``````````````````````````.``````.++-``s.```````-/:``.//.``````````````````````````"
+sleep(0.03)
+puts "NNNNNNNNMMMNNNNNNNNNNNNNNNNMNNNNNNNNNNNN+:::::::::oh../ssssssyd/``````````......``````````````````````.y/`.s`````-o/../s+.```````````````````````....."
+sleep(0.03)
+puts "NNNNNNNNMNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNN/:::::::::m-:ssssssssh:````.:/+oossssssssso+/:-.```````````````:h``yo.``/+``.h/`````````````````.-:/+ossssssss"
+sleep(0.03)
+puts "NNNNNNMMMNNNNNNNNNNNNNNNNNMNNNNNNNNNNNNN+::::::::+dosssssssssdo```/ssssssyhhhhhhdhhyyssss+/-.``````````-m-``oh`.s```ss`````````````.-/osysssyyyhhhhhhh"
+sleep(0.03)
+puts "NNNNNNNMMNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNs::::::::omssssssssyd+y.``.osssssssyhddhysssyyyhhhysso/-````````+d:`.d-h+``:m-`````````.:+osssyhddhdmmmdhyssss"
+sleep(0.03)
+puts "NNNNNMMMNNNNNNNNNNNNNNNNNMNNNNNNNNNNNNNNm/:::::/osdhsssssshy::+:````-/ssydhs/::::::::::::/sydhyss+-.`````-m+`:d/``+m:``````.:/osyyhdhys+:::::::/oydmhs"
+sleep(0.03)
+puts "NNNNMMMNNNNNNNNNNNNNNNNMMNNNNNNNNNNNNNNNNh::::+sssshdhysyd+::-.`````-symy/:::--.--:::::::::::/shdyss+-````oN.+y``+m-````./ossyhdho/:::::::::::::::::od"
+sleep(0.03)
+puts "NNNNMMNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNh::osssssssyhmh/::-``````-sdd/::-.`      `..--::::::::/sdhsss//+sN/sd++hNs+//ossshdy+::::::::::::::--...-:::"
+sleep(0.03)
+puts "NNNNNNNNNNNNNNNNNNNNNNNMNNNNNNNNNNNNNNNNNNNdoosssssssshs:::-``````.sms::-.              `.-::::::::yNdhyyyddsymyssdmhysyhdNo:::::::::-..``          .:"
+sleep(0.03)
+puts "NNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNdssssssyy+::::.`````.omy::-`                   .-::::::dhhddhysssyysssyhdddhms:::::--.`                 ."
+sleep(0.03)
+puts "NNNMNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNmhhhy/:::::.``````:hN:::`                      `-::::odsssso++////++ossssym::::.`                      "
+sleep(0.03)
+puts "NNNNNNNNNNNNNNNNNNNNNMNNNNNNNNNNNNNNNNNNNNNNNNmhs/:::::::.```````+dd::-                         -:::ods+-.``````````.-+sym:::                         "
+sleep(0.03)
+puts "NNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNmyo/:::::::::-.````````/hm::-                         ::::yy.````````````````./m/:-                         "
+sleep(0.03)
+puts "MNNNNNNNNNNNNNNNNNNNMNNNNNNNNNNNNNNNNNdy+:::::::::::::-````.////::ym/:-                        .:::/m-```````````````````od::.                    `-+o"
+sleep(0.03)
+puts "NNNNNNNNNNNNNNNNNNMNNNNNNNNNNNNNNNNho:::::::::/ossoooo++os+///+ossyyyyso+++/-.`               .:::+m/````````````````````.dy::.               `:oso+//"
+sleep(0.03)
+puts "NNNNNNNNNNNNNNNNNMNNNNNNNNNNNNNNho::::::::::+s+-``.://:.`-+o/`````...-:/++++shhyoo+/:`      `-::+hy-``````````````````````.dd/:.          `:ooo//:/:.`"
+sleep(0.03)
+puts "NNNNNNNNNNNNNNNNNNMNNNNNNNNNNms:::::::::::/s+``./hmmmmmmdys+:.````````````````.-////o+ooo+/::+ydy:`````````````````````````.sdy/-.     `/oo:.-:-``````"
+sleep(0.03)
+puts "NNNNNNNNNNNNNNNNMNNNNNNNNNNms/:::::::::::+y.`.+dmdddhddmd::+syss+:.``````````````````````.-+sddo-````````````````````````````-oddyo:-+o+-`````````````"
+sleep(0.03)
+puts "NNNNNNNNNNNNNNNNMNNNNNNNNmy+::::::::::::oy.`:dmddddhhdmd/-.....-/+sso+/.``````````````````````.-+so/.``````````````````````````.-omd+-````````````````"
+sleep(0.03)
+puts "NNNNNNNNNNNNNNNMMNNNNNNNho/:::::::::::-/y.`oNmddhhhhdmN/`        ``.-:+osso/-.`````````````````````-:.`````````````````````````:o+-``````````````.-/oo"
+sleep(0.03)
+puts "NNNNNNNNNNNNNNMNNNNNNNNso/::::::::::-.-h..yNdddhhhhhdmh.             `.:::m+ossso+:-``````````````````````````````````````````````````````.:/oossso+ho"
+sleep(0.03)
+puts "NNNNNNNNNNNNNNNNNNNNNNso/:::::::::-.``y-.dmddddddddddNy`               `:yo:-...-:+ssssoo/:-.````````````````````````````````...-:/+oosssso+/-.....-/m"
+sleep(0.03)
+puts "NNNNNNNNNNNNNNNNNNNNNss+:::::::::.````..dmddddmdyyyhdNN/               `/d-`        ``.-:/ydooo+oooooooooooooooooooooooooossssssosm/:-.``           `y"
+sleep(0.03)
+puts "NNNNNNNNNNNNNNNNNNNNyoo::::::::-.`````.dmddmd+`      `/sy/`            -d/              `:oy:-......```...-:::y+:::--..........-::yh.                -"
+sleep(0.03)
+puts "NNNNNNNNNNNNNNNNNNNmos/:::::::-```````:Nmdms`            -os/`        -hy`               .yy.               .:ys-`              `-/N:                 "
+sleep(0.03)
+puts "NNNNNNNNNNNNNNNNNNMsso:::::::-````````.hmm+.                -odyso+ooyNMo-               -m:                 -ss`                `:mo                 "
+sleep(0.03)
+puts "NNNNNNNNNNNNNNNNNNmoso::::::-``````````./yo:.                :hd+:.``.-/oss+:.          -ds.                 .yy`                 :do              `-+"
+sleep(0.03)
+puts "NNNNNNNNNNNMNNNNNNhsss::::::.`````````````-oso:``          .yy-            `./++o/.  `:sNhoo///:-.`          -yy.                `/No+/++oooo/.  /s+-`"
+sleep(0.03)
+puts "NNNNNNNNNNMNNNNNNNssss/::::.`````````````````-oyy+-..```..:d+`                   /NNdo/-..`..--://ooss/`    -+ddsoooooooo+:`  `:oo+/:-.``    -hshs    "
+sleep(0.03)
+puts "NNNNNNNNNNNNNNNNNNsssso:::-`````````````````````.:osss+/::ho:`                    om`                `/hysss+:.          ./hysy-               oM/    "
+sleep(0.03)
+puts "NNNNNNNNNNNNNNNNNMsssss/::.``````````````````````````.:+osdso/-.``                sy                   `mm:                 oM/                 ds`   "
+sleep(0.03)
+puts "NNNNNNNNNMNNNNNNNNhossso:-`````````````````````````````````.-/ossys+:-.``        .sy`                  .do                   ym`                +N-   "
+sleep(0.03)
+puts "NNNNNNNNNNNNNNNNNNNosssso-````````````````````````````````````````.-/ossyso/:----:do-                  -N/                   :N+`               :N+:.`"
+sleep(0.03)
+puts "NNNNNNNNNNNNNNNNNNNhssssso.``````````````````````````````````````````````.-/osyyyyNo/:-..``           .sm:`                  -dh-.`       ```.--:No++o"
+sleep(0.03)
+puts "NNNNNNNNNNNNNNNNNNNNsssssss:````````````````````````````````````````````````````.-:/ossyyyyyso++//::::/mo::-..```````````..-/+Nh++ooooosyyhhhyyyso+/:-"
+sleep(0.03)
+puts "NNNNNNNNNNNNNNNNNNNNmosssssso-````````````````````````````````````````````````````````````..-://+oossyyhhhhhdddddddddhhhhhhyyysooo++//:--..``.-.``````"
+sleep(0.03)
+puts "NNNNNNNNNmmmNNNNNNNNNdossssssss+-.`````````````````````````````````````````````````````./osoo++///::::----......````````.....--:::/++ossssoo+:.```````"
+sleep(0.03)
+puts "NNNNNMNdyyyyyhhddmmmNNdysssssssssso/-.````````````.````````````````````````````````````````.-::/+oosyyhhhhhhhhhhhhhhhhhddddhhhyysoo+/:--.`````````````"
+sleep(0.03)
+puts "NNNNNNmmmdddhhhhhyyyyhhdddddddhhyssssso+/:..``````````````````````````````````````````````````````````....-----::::::----....`````````````````````````"
+sleep(0.03)
+puts "hhddmmmNmmmmmmmmmmddddhhhhhyyhhhddddddhhyyssso+/:--..`````````````````````````````````````````````````````````````````````````````````````````````````"
+sleep(0.03)
+puts "yyyyyyyhhhdddmmmmNNmmmmmmmmmddddhhhyyhhhddddddhhyyssssoo+//:::--...``````````````````````````````````````````````````````````````````````````````....."
+sleep(0.03)
+puts "yyyyyyyyyyyyyyyyyhhhdddmmmmNNmmmmmmmddddhhhhyhhhddddddhyyssssssssssssooo++++////:::::::::--------------------:::::::::::::////////+++++++ooooossssssss"
+
+# SUPPRESION DU CONTENU DE LA TABLE SI BESOIN ET DEMARRAGE DU SEED.
 City.destroy_all
 Doctor.destroy_all
 Patient.destroy_all
@@ -15,35 +104,61 @@ ActiveRecord::Base.connection.execute("DELETE from sqlite_sequence where name = 
 ActiveRecord::Base.connection.execute("DELETE from sqlite_sequence where name = 'specialties'")
 ActiveRecord::Base.connection.execute("DELETE from sqlite_sequence where name = 'spe_docs'")
 
-puts `clear`
-puts "Ecriture de la base de données..."
+puts ""
+puts "                                                                                    -- DATABASE #{"REINITIALIZED".colorize(:red).blink} --           You've been hard_peepooded!"
 sleep(2)
+
 10.times do
 	city = City.create!(name: Faker::Pokemon.location)
-	puts "City : #{city.name}"
+	print "."
 end
+puts "Wait for it..."
+sleep(1)
 
 10.times do
 	doctor = Doctor.create!(first_name: Faker::BreakingBad.character, last_name: Faker::Superhero.suffix, zip_code: Faker::HarryPotter.house, city_id: City.all.sample.id)
-	puts "Doctor : #{doctor.first_name} | #{doctor.last_name} | #{doctor.zip_code} | #{doctor.city_id}"
+	print "."
 end
+puts "Wait for it..."
+sleep(1)
 
 50.times do
 	patient = Patient.create!(first_name: Faker::Superhero.prefix, last_name: Faker::GameOfThrones.character, city_id: City.all.sample.id)
-	puts "Patient : #{patient.first_name} | #{patient.last_name} | #{patient.city_id}"
+	print "."
 end
+puts "Wait for it..."
+sleep(1)
 
 70.times do
 	appointment = Appointment.create!(date: Faker::Date.forward(365), city_id: City.all.sample.id, doctor_id: Doctor.all.sample.id, patient_id: Patient.all.sample.id)
-	puts "Appointment : #{appointment.date} | #{appointment.city_id} | #{appointment.doctor_id} | #{appointment.patient_id}"
+	print "."
 end
+puts "Wait for it..."
+sleep(1)
 
 10.times do
 	specialty = Specialty.create!(specialty: Faker::Beer.name)
-	puts "Specialty : #{specialty.specialty}"
+	print "."
 end
+puts "Wait for it..."
+sleep(1)
 
 20.times do
 	spe_doc = SpeDoc.create!(doctor_id: Doctor.all.sample.id, specialty_id: Specialty.all.sample.id)
-	puts "SpeDoc : #{spe_doc.doctor_id} | #{spe_doc.specialty_id}"
+	print "."
 end
+
+puts ""
+
+puts "\u{1F4A9}" * 75
+
+puts ""
+sleep(2)
+puts "                                                                                                      DONE !! NOW ENJOY.."
+
+puts ""
+sleep(2)
+puts "                                                                                                   AND GET READY TO CTRL + C...."
+sleep(3)
+
+system("curl parrot.live")
